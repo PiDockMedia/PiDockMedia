aa36b6b174530da8d6bfd25a00791d0cda4c4438
PiDockMedia <pidockmedia@gmail.com>
2019-06-04 19:28:29 -0400

    Added some more early stuff

--- pifirstboot.sh
+++ pifirstboot.sh
@@ -1,6 +1,54 @@
 #!/bin/bash
 
-# This script will run on first boot of the Pi and then destroy itself.
+# This script does stuff to your Raspbian image
+#
+# By default it just sets SSH to on
 
+# To add your own:
+#
+# Declarations      - set any default variables
+# FunctionsGenerate - create a function to generate what you need but don't do anything
+
+# FunctionDescribe - put a line in saying what is being done
+
+##### Declarations - things you might want to hard code
+BootDirPath=""
+
+##### FunctionsGenerate
+
+SSHEnable()
+{
+while [ !-f "$BootDirPath/kernel.img" ]; do
+    read -p "Type the full path to the /boot directory (e.g. /media/boot)" BootDirPath
+done
+}
+
+
+# FunctionsDo
+
+
+
+##### FunctionDescribe
+
+DescribeWhatYouAreDoing()
+{
+    echo "Dropping SSH in $BootDirPath"
+}
+
+
+done
+
+
+VerifyAndDo()
+{
+while true; do
+    DescribeWhatYouAreDoing
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+}
 
 ###EOF
